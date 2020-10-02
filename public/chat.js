70797101671efa2af300e585432b56705c6963e3
Saptarshi Basu <123saptarshi.basu@gmail.com>
2020-10-02 18:31:55 +0530

    Adding custom favicon and some document formatting

--- public/chat.js
+++ public/chat.js
@@ -1,45 +1,45 @@
 const socket = io()
 
 function formatDate(date) {
-  date = new Date(date)
+    date = new Date(date)
 
-  const hours = date.getHours()
-  const minutes = date.getMinutes()
+    const hours = date.getHours()
+    const minutes = date.getMinutes()
 
-  return `[${hours}:${String(minutes).split('').length === 2 ? minutes : '0' + minutes}]`
+    return `[${hours}:${String(minutes).split('').length === 2 ? minutes : '0' + minutes}]`
 }
 
 function renderMessage(message) {
-  const messages = document.getElementById('messages')
-  
-//added minor styles for cleaner effect
-  messages.innerHTML += (`
+    const messages = document.getElementById('messages')
+
+    //added minor styles for cleaner effect
+    messages.innerHTML += (`
     <div class="message">
         <span class="date">${formatDate(message.date)}</span> <b>${message.author}</b>: ${message.content}
     </div>
   `)
 
-  messages.scrollTop = messages.scrollHeight
+    messages.scrollTop = messages.scrollHeight
 }
 
 socket.on('recivedMessage', message => {
-  renderMessage(message)
+    renderMessage(message)
 })
 
 document.forms['chat'].addEventListener('submit', event => {
-  event.preventDefault()
+    event.preventDefault()
 
-  const message = new Object
+    const message = new Object
 
-  message.author = document.getElementById('username').value
-  message.content = document.getElementById('message_content').value
-  message.date = new Date
+    message.author = document.getElementById('username').value
+    message.content = document.getElementById('message_content').value
+    message.date = new Date
 
-  if (message.author.length && message.content.length) {
-    document.getElementById('message_content').value = ''
+    if (message.author.length && message.content.length) {
+        document.getElementById('message_content').value = ''
 
-    renderMessage(message)
+        renderMessage(message)
 
-    socket.emit('sendMessage', message)
-  }
+        socket.emit('sendMessage', message)
+    }
 })
